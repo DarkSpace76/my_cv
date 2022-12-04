b44620f2bf5f77f1277adc23dc0a3ee8bc0bb60c
DarkSpace76 <84933249+DarkSpace76@users.noreply.github.com>
2022-12-04 09:21:02 +0300

    feat: add user card

---
+++ lib/components/user_box.dart
@@ -0,0 +1,39 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+Widget userBox({EdgeInsets? margin}) {
+  return ClipRect(
+      child: BackdropFilter(
+    filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+    child: Container(
+      constraints: BoxConstraints(
+          maxHeight: 500, minHeight: 500, maxWidth: 300, minWidth: 300),
+      margin: margin,
+      decoration: BoxDecoration(color: Colors.grey.shade700.withOpacity(0.2)),
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 200,
+              width: 200,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(360)),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Alexandr Syrokvash',
+              style: TextStyle(fontSize: 25, color: Colors.white),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Flutter Developer',
+              style:
+                  TextStyle(fontSize: 15, color: Colors.white.withOpacity(.5)),
+            ),
+          ]),
+    ),
+  ));
+}
