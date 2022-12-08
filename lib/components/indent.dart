6473e3b5cca3c61f98730df8e95d1cb5809e132b
DarkSpace76 <84933249+DarkSpace76@users.noreply.github.com>
2022-12-08 13:33:03 +0300

    refactoring: remove files

---
+++ lib/components/indent.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:my_resume/const.dart';
+
+horizontalPadding({double width = 30}) {
+  return SizedBox(width: width);
+}
+
+verticalPadding({double height = 30}) {
+  return SizedBox(height: height);
+}
+
+devider() {
+  return Container(
+    width: double.infinity,
+    height: 1,
+    padding: const EdgeInsets.symmetric(horizontal: 50),
+    margin: const EdgeInsets.symmetric(vertical: 30),
+    color: bgColor2.withOpacity(0.2),
+  );
+}
